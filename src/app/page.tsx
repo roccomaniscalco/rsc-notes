4f81de73c9d76814955d1d80a4cfaff89b7f2486
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-05-09 03:23:49 -0400

    display and create todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Index() {
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+
+export default async function Index() {
+  const { rows: todos } = await sql`SELECT * from TODO`;
+
+  async function createTodo (todoFormData: FormData) {
+    "use server"
+
+    const content = todoFormData.get("content") as string
+    if (!content) return
+
+    await sql`insert into todo (content) values (${content});`
+    revalidatePath("/")
+  }
+
   return (
     <main className="max-w-4xl mx-auto my-4">
-      <h1>Hello, World!</h1>
+      <h1>TODOS</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <form action={createTodo}>
+          <input type="text" name="content" />
+          <button type="submit">Create Todo</button>
+      </form>
     </main>
   );
 }
