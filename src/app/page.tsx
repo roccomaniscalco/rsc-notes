1a0fed4e63803f5324f51a27ce7884e8bf434991
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-05-09 02:23:29 -0400

    add main with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
+export default function Index() {
   return (
-    <h1>Hello, World!</h1>
+    <main className="max-w-4xl mx-auto my-4">
+      <h1>Hello, World!</h1>
+    </main>
   );
 }
